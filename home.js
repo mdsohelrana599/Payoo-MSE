2f56f712b15a4a679b0ccdeeb424e765a32a5d15
mdsohelrana599 <mdsohelrana812005@gmail.com>
2025-08-23 21:53:58 +0600

    6th commit

--- home.js
+++ home.js
@@ -1,1 +1,36 @@
-console.log('hello')
+const validpin = 7005;
+
+// add money feature
+
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    const bank = document.getElementById("bank").value;
+    const accountNumber = document.getElementById("account-number").value;
+
+    const amount = parseInt(document.getElementById("add-amount").value);
+
+    const pinNumber = parseInt(document.getElementById("add-pin").value);
+
+    const availableBalance = parseInt(
+      document.getElementById("available-balance").innerText
+    );
+
+    if (accountNumber.length < 11) {
+      alert("Inaild account Number");
+      return;
+    }
+    if (pinNumber !== validpin) {
+      alert("Invalid pin Number");
+      return;
+    }
+
+    const totalNewavailableBalance = amount + availableBalance;
+
+    document.getElementById("available-balance").innerText =
+      totalNewavailableBalance;
+  });
+
+
+//   toggling feature 
